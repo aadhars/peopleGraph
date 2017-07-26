fd327a5cabbff3d1fbf22b19a35e693066850daf
Akshansh Srivastava <aksriv@microsoft.com>
2017-07-26 12:45:08 +0530

    Extension checkin with full code

---
+++ TimelineExtension/scripts/LoadData.js
@@ -0,0 +1,186 @@
+//module 
+define(["require", "exports", "VSS/Authentication/Services"], function (require, exports, Services) {
+    return {
+        workItemObjects: [],
+        getWorkItemData: function (url) {
+            var deferred = $.Deferred();
+            var that = this;
+            $.ajax({
+
+                url: url,
+
+                type: "GET",
+
+                contentType: "application/json",
+
+                success: function (workItemDetails) {
+
+                    //console.log(c.dashboardEntries);
+
+                    deferred.resolve(workItemDetails);
+
+                },
+
+                error: function (e) {
+
+                    //TelemetryClient.getClient().trackException(e.response);
+
+                }
+
+            });
+
+            deferred.promise().then(function (workItemDetails) {
+                //Convert to JSON to extract link wise data
+                let workItemObject = {
+                    "url": workItemDetails.url,
+                    "id": workItemDetails.id,
+                    "name": workItemDetails.fields["System.Title"],
+                    "type": workItemDetails.fields["System.WorkItemType"],
+                    "events": []
+                }
+
+                //Get updates data
+                $.ajax({
+                    url: workItemDetails._links.workItemUpdates.href,
+
+                    type: "GET",
+
+                    contentType: "application/json",
+
+                    success: function (workItemUpdates) {
+
+                        //console.log(c.dashboardEntries);
+                        workItemUpdates.value.forEach(function (workItemUpdateValue) {
+                            let workItemEvent = "WorkItemEvent";
+                            if (workItemUpdateValue.fields["System.State"]) {
+                                if (workItemUpdateValue.fields["System.State"].newValue === "Closed") {
+                                    workItemEvent = "WorkItemCompletedEvent";
+                                }
+                            }
+                            let reason = workItemUpdateValue.fields["System.Reason"];
+                            if (reason) {
+                                //IF reason exists, then only capture the event, else ignore the event
+                                workItemObject.events.push({
+                                    "type": workItemEvent,
+                                    "startTime": workItemUpdateValue.fields["System.ChangedDate"].newValue,
+                                    "name": reason.newValue,
+                                    "url": workItemDetails.url
+                                });
+                            }
+                        });
+
+                        that.workItemObjects.push(workItemObject);
+                        console.log(that.workItemObjects);
+                    },
+
+                    error: function (e) {
+
+                        //TelemetryClient.getClient().trackException(e.response);
+
+                    }
+                });
+
+
+            });
+        },
+
+        dataLoader: function () {
+            var deferred = $.Deferred();
+            //Get the Access token from SDK
+            let authTokenManager = Services.authTokenManager;
+            VSS.getAccessToken().then(function (token) {
+                //Get bearer token
+                let header = authTokenManager.getAuthorizationHeader(token);
+                $.ajaxSetup({
+
+                    headers: { 'Authorization': header }
+
+                });
+
+                var collectionUri = VSS.getWebContext().collection.uri;
+                var apiURL = collectionUri + VSS.getWebContext().project.id + "/" + VSS.getWebContext().team.id + "/_apis/wit/wiql?api-version=1.0";
+                var postData = { "query": `SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.AssignedTo] = @me AND [System.WorkItemType] IN ("Task", "User Story") AND [State] IN ("Active", "Closed")` };
+
+                console.log(apiURL);
+                $.ajax({
+
+                    url: apiURL,
+
+                    type: "POST",
+
+                    contentType: "application/json",
+
+                    data: JSON.stringify(postData),
+
+                    success: function (c) {
+
+                        //console.log(c.dashboardEntries);
+
+                        deferred.resolve(c.workItems);
+
+                    },
+
+                    error: function (e) {
+
+                        //TelemetryClient.getClient().trackException(e.response);
+
+                    }
+
+                });
+
+            });
+
+            var that = this;
+            deferred.promise().then(function (workItems) {
+                //Convert to JSON to extract link wise data
+                workItems.forEach(function (workItem) {
+                    that.getWorkItemData(workItem.url);
+                }, this);
+
+            });
+        },
+        showData: function (serverData) {
+            var container = document.getElementById('visualization');
+
+
+
+            // Items
+
+            //var data = convertDataToVISData(getMockDataSet());
+
+            var data = convertDataToVISData(serverData);
+
+
+
+            // Options
+
+            var options = {
+
+                width: '100%',
+
+                margin: {
+
+                    item: 20
+
+                },
+
+                orientation: {
+
+                    axis: "top"
+
+                },
+
+                stack: false
+
+            };
+
+
+
+            // Timeline
+
+            var timeline = new vis.Timeline(container, data.items, data.groups, options);
+
+        }
+    };
+}
+);
---
+++ TimelineExtension/scripts/helper.js
@@ -0,0 +1,391 @@
+function getClassName(eventType) {
+
+
+
+    if (eventType === "WorkItemEvent") {
+
+        return "workitem-request-bar circle-of-death";
+
+    }
+
+    else if (eventType === "PullRequestedEvent") {
+
+        return "pull-request-bar circle-of-death";
+
+    }
+
+    else if (eventType === "WorkItemCompletedEvent") {
+
+        return "workitem-request-bar circle-of-death";
+
+    }
+
+}
+
+
+
+function addMinutes(date, minutes) {
+    var dateObject = new Date(date);
+    return new Date(dateObject.getTime() + minutes * 60000);
+
+}
+
+
+
+function convertDataToVISData(data) {
+
+
+
+    var groups = new vis.DataSet();
+
+    var items = new vis.DataSet();
+
+
+
+    var startIndex = 0;
+
+
+
+    for (var index = 0; index < data.length; index++) {
+
+        groups.add({ id: data[index].id, content: data[index].name });
+
+
+
+        var events = data[index].events;
+
+        var firstEvent = events[0];
+
+        var endTime;
+
+        if (true || events[events.length - 1].type === "WorkItemCompletedEvent") {
+
+            endTime = addMinutes(events[events.length - 1].startTime, 0);
+
+        }
+
+
+
+        items.add({
+
+            id: startIndex++,
+
+            group: data[index].id,
+
+            content: '',
+
+            start: firstEvent.startTime,
+
+            end: endTime,
+
+            className: "workitem-container"
+
+        });
+
+
+
+        for (var eIndex = 0; eIndex < events.length; eIndex++) {
+
+
+
+            var event = events[eIndex];
+
+
+
+            items.add({
+
+                id: startIndex++,
+
+                group: data[index].id,
+
+                content: getContentData(event.type),
+
+                title: getTitleHtml(event.type, event, data[index].name),
+
+                start: event.startTime,
+
+                end: addMinutes(event.startTime, 30),
+
+                className: getClassName(event.type)
+
+            });
+
+        }
+
+    }
+
+
+
+    items = getMettingSchedules(items);
+
+    // items.add(...schedules);
+
+
+
+    return {
+
+        items: items,
+
+        groups: groups
+
+    }
+
+}
+
+
+
+function getMettingSchedules(items) {
+
+
+
+    //  var items = new vis.DataSet();
+
+
+
+    // items.add({
+
+    //     id: "A",
+
+    //     content: "Team Scrum",
+
+    //     type: "background",
+
+    //     start: new Date(2017, 7, 25, 10, 30, 0),
+
+    //     end: new Date(2017, 7, 25, 11, 30, 0)
+
+    // });
+
+
+
+    // items.add({
+
+    //     id: "B",
+
+    //     content: "Bug Bash",
+
+    //     type: "background",
+
+    //     start: new Date(2017, 7, 25, 15, 30, 0),
+
+    //     end: new Date(2017, 7, 25, 17, 30, 0)
+
+    // });
+
+
+
+    return items;
+
+}
+
+
+
+function getContentData(eventType, url) {
+
+    if (eventType === "WorkItemEvent" || eventType === "WorkItemCompletedEvent") {
+
+        return "W";
+
+    }
+
+    else if (eventType === "PullRequestedEvent") {
+
+        return "P";
+
+    }
+
+}
+
+
+
+function getTitleHtml(eventType, event, workitemName) {
+
+
+
+    if (eventType === "WorkItemEvent" || eventType === "WorkItemCompletedEvent") {
+
+        var html = '<div class="workitem-card">'+ 
+
+                       '<div class="workitem-card-name">'+workitemName + '</div>'+
+
+                       '<div class="workitem-card-event-type">Event:'+ event.name + '</div>'+
+
+                     '</div>'  
+
+        return html;
+
+    }
+
+    else {
+
+        return event.name;
+
+    }
+
+
+
+}
+
+
+
+function getMockDataSet() {
+
+    return [
+
+        {
+
+            id: 1,
+
+            url: "http://www.mseng.visualstudio.com",
+
+            name: "Task Item 1",
+
+            type: "Task",
+
+            events: [
+
+                {
+
+                    type: "WorkItemEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 1 created",
+
+                    startTime: new Date(2017, 7, 25, 10, 0, 0)
+
+                },
+
+                {
+
+                    type: "WorkItemEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 1 move to in progress",
+
+                    startTime: new Date(2017, 7, 25, 10, 30, 0)
+
+                },
+
+                {
+
+                    type: "PullRequestedEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "Pull request 1 created",
+
+                    startTime: new Date(2017, 7, 25, 12, 0, 0)
+
+                },
+
+                {
+
+                    type: "PullRequestedEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "pull request 1 completed",
+
+                    startTime: new Date(2017, 7, 25, 15, 0, 0)
+
+                },
+
+                {
+
+                    type: "WorkItemEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 1 completed",
+
+                    startTime: new Date(2017, 7, 25, 18, 0, 0)
+
+                },
+
+
+
+            ]
+
+        },
+
+        {
+
+            id: 2,
+
+            url: "http://www.mseng.visualstudio.com",
+
+            name: "Task Item 2",
+
+            type: "Task",
+
+            events: [
+
+                {
+
+                    type: "WorkItemEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 2 created",
+
+                    startTime: new Date(2017, 7, 25, 9, 0, 0)
+
+                },
+
+                {
+
+                    type: "WorkItemEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 2 move to in progress",
+
+                    startTime: new Date(2017, 7, 25, 9, 30, 0)
+
+                },
+
+                {
+
+                    type: "PullRequestedEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "Pull request 2 created",
+
+                    startTime: new Date(2017, 7, 25, 11, 0, 0)
+
+                },
+
+                {
+
+                    type: "PullRequestedEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "pull request 2 completed",
+
+                    startTime: new Date(2017, 7, 25, 14, 0, 0)
+
+                },
+
+                {
+
+                    type: "WorkItemCompletedEvent",
+
+                    url: "http://www.mseng.visualstudio.com",
+
+                    name: "User story 2 completed",
+
+                    startTime: new Date(2017, 7, 25, 17, 0, 0)
+
+                },
+
+            ]
+
+        }
+
+    ];
+
+}
